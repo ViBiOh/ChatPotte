d05c9cd417fd43897cd24efc3390d42abfd55dee
Vincent Boutour <bob@vibioh.fr>
2023-05-28 20:46:27 +0200

    refactor(flags): Updating usage
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/discord/main.go
+++ cmd/discord/main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	fs := flag.NewFlagSet("discord", flag.ExitOnError)
+	fs.Usage = flags.Usage(fs)
 
 	loggerConfig := logger.Flags(fs, "logger")
 	discordConfig := discord.Flags(fs, "")
