f6ef1a6d6e03dd946d08cb54de5dbec862b9e2f0
Vincent Boutour <bob@vibioh.fr>
2023-08-09 10:05:40 +0200

    fix: Fixing double env variable being set
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/discord/main.go
+++ cmd/discord/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	loggerConfig := logger.Flags(fs, "logger")
 	discordConfig := discord.Flags(fs, "")
-	configuration := flags.New("commands", "Configuration of commands, as JSON string").Prefix("commands").String(fs, "", nil)
+	configuration := flags.New("", "Configuration of commands, as JSON string").Prefix("commands").String(fs, "", nil)
 
 	logger.Fatal(fs.Parse(os.Args[1:]))
 
