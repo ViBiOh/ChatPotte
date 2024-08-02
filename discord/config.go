09160d688a54ec5bc968ff5720e9396259169578
Vincent Boutour <bob@vibioh.fr>
2024-08-02 23:53:59 +0200

    refactor: Isolating the signin client
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- discord/config.go
+++ discord/config.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ViBiOh/httputils/v4/pkg/httpjson"
+	"github.com/ViBiOh/httputils/v4/pkg/request"
 )
 
 func (s Service) ConfigureCommands(ctx context.Context, commands map[string]Command) error {
@@ -16,21 +17,11 @@ func (s Service) ConfigureCommands(ctx context.Context, commands map[string]Comm
 		return nil
 	}
 
-	data := url.Values{}
-	data.Add("grant_type", "client_credentials")
-	data.Add("scope", "applications.commands.update")
-
-	resp, err := discordRequest.Method(http.MethodPost).Path("/oauth2/token").BasicAuth(s.clientID, s.clientSecret).Form(ctx, data)
+	req, err := s.SigninClient(ctx)
 	if err != nil {
-		return fmt.Errorf("get token: %w", err)
+		return fmt.Errorf("signin: %w", err)
 	}
 
-	content := make(map[string]any)
-	if err := httpjson.Read(resp, &content); err != nil {
-		return fmt.Errorf("read oauth token: %w", err)
-	}
-
-	bearer := content["access_token"].(string)
 	rootURL := fmt.Sprintf("/applications/%s", s.applicationID)
 
 	for name, command := range commands {
@@ -38,7 +29,7 @@ func (s Service) ConfigureCommands(ctx context.Context, commands map[string]Comm
 			absoluteURL := rootURL + registerURL
 
 		configure:
-			if resp, err := discordRequest.Method(http.MethodPost).Path(absoluteURL).Header("Authorization", fmt.Sprintf("Bearer %s", bearer)).StreamJSON(ctx, command); err != nil {
+			if resp, err := req.Method(http.MethodPost).Path(absoluteURL).StreamJSON(ctx, command); err != nil {
 				if resp.StatusCode == http.StatusTooManyRequests {
 					slog.LogAttrs(ctx, slog.LevelWarn, "Rate-limited, waiting 5 seconds before retrying...", slog.String("url", absoluteURL))
 					time.Sleep(time.Second * 5)
@@ -54,6 +45,26 @@ func (s Service) ConfigureCommands(ctx context.Context, commands map[string]Comm
 	}
 
 	return nil
+}
+
+func (s Service) SigninClient(ctx context.Context) (request.Request, error) {
+	data := url.Values{}
+	data.Add("grant_type", "client_credentials")
+	data.Add("scope", "applications.commands.update")
+
+	resp, err := discordRequest.Method(http.MethodPost).Path("/oauth2/token").BasicAuth(s.clientID, s.clientSecret).Form(ctx, data)
+	if err != nil {
+		return discordRequest, fmt.Errorf("get token: %w", err)
+	}
+
+	content := make(map[string]any)
+	if err := httpjson.Read(resp, &content); err != nil {
+		return discordRequest, fmt.Errorf("read oauth token: %w", err)
+	}
+
+	bearer := content["access_token"].(string)
+
+	return discordRequest.Header("authorization", fmt.Sprintf("Bearer %s", bearer)), nil
 }
 
 func getRegisterURLs(command Command) []string {
